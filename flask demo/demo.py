03eb339896c85335616fce1be4ba469da2774188
Lisbeth Caxaj <Lisbethcaxaj200@gmail.com>
2026-03-26 23:51:31 -0500

    Add files via upload

---
+++ flask demo/demo.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/submit", methods=["POST"])
+def submit():
+    name = request.form["name"]
+    return f"Hello, my name is {name} and I'm learning Flask!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
